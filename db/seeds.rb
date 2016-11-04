5faac700319b493b7a7e22524ab0f3e0134b4a74
Greg <szabogergely07@gmail.com>
2016-11-04 10:06:10 +0100

    4.2 Add color field to Products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(:name => "seed-generated product", :image_url => "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRXtjA2wOxC1RzjyaDDfUiIyMiZllmpu6Fzl3MOGAruOBh2tfelgw", :description => "I also created this product without using the HTML form!")
