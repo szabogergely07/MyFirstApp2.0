62292f318c63157e25b654bfcaf80d4e11c402e0
Greg <szabogergely07@gmail.com>
2016-11-09 16:28:57 +0100

    4.5 Change amount of products in carousel depends on total amount of products

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,9 +2,18 @@ class StaticPagesController < ApplicationController
   def index
   end
 
-
+  # Dinamically show products in carousel depends on total amount
 	def landing_page
-		@products = Product.limit(3)
+		def x
+			if Product.count <= 3
+				x = 3	
+			elsif
+				Product.count < 10
+				x = 4
+			else
+				x = 5
+			end
+		end
+	@products = Product.limit(x)
 	end
-
 end
