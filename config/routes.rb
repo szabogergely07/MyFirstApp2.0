f731312ebdec60fd592af19c8cbe499dfa5837ce
Greg <szabogergely07@gmail.com>
2016-11-24 17:37:55 +0100

    5.3 Transform contact page to be dynamic with thank_you template

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
 
   get 'static_pages/landing_page'
 
+  post 'static_pages/thank_you'
+
   resources :orders, only: [:index, :show, :create, :destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
