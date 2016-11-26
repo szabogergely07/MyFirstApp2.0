aaed24bbaedc27544ddd8f97f097f2f7f53e7d28
Greg <szabogergely07@gmail.com>
2016-11-26 13:53:01 +0100

    5.3 Correct credentials

--- config/environment.rb
+++ config/environment.rb
@@ -9,8 +9,8 @@ ActionMailer::Base.smtp_settings = {
   :address => 'smtp.sendgrid.net',
   :port => '587',
   :authentication => :plain,
-  :user_name => ENV['app58362653@heroku.com'],
-  :password => ENV['Stairs77'],
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
   :domain => 'heroku.com',
   :enable_starttls_auto => true
 }
