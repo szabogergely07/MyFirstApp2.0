189b86a8c8da44ae82978a9eb4b362ce28a35e47
Greg <szabogergely07@gmail.com>
2016-11-26 12:54:36 +0100

    5.3 Create UserMailer

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: 'szabogergely07@gmail.com'
   layout 'mailer'
 end
---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,10 @@
+class UserMailer < ApplicationMailer
+	default from: "szabogergely07@gmail.com"
+
+  	def contact_form(email, name, message)
+  	@message = message
+    mail(:from => email,
+        :to => 'your-email@example.com',
+        :subject => "A new contact form message from #{name}")
+  end
+end
