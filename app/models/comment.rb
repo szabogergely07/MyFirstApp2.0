72535e77c07d1ad77a9353bda0b1a8a1d5c246e6
Greg <szabogergely07@gmail.com>
2016-12-14 09:51:03 +0100

    5.7 Create lowest rating product and style product show page

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,5 @@ class Comment < ApplicationRecord
   belongs_to :product
 
   scope :rating_desc, -> { order(rating: :desc) }
+  scope :rating_asc, -> { order(rating: :asc) }
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,8 @@ class Product < ApplicationRecord
 	def highest_rating_comment
 		comments.rating_desc.first
 	end
+
+	def lowest_rating_comment
+		comments.rating_asc.first
+	end
 end
