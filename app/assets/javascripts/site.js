049af6ffec9d648a7db6c0bc23b5695a61bbf189
Greg <szabogergely07@gmail.com>
2016-12-17 13:35:30 +0100

    5.8 Correct comments controller

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,5 +1,5 @@
 $(document).on('turbolinks:load', function(){
-    $('.rating').raty({ path: '/assets', scoreName: 'comment[rating]' });
+    $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
     $('.rated').raty({ path: '/assets',
       readOnly: true,
       score: function() {
