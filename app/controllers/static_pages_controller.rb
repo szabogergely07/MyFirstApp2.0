c9cf1cf4d0a973f9b3738bfeec5dc452df8717ca
Greg <szabogergely07@gmail.com>
2017-01-09 11:30:52 +0100

    5.11 Fix issue: relocate landing_page action from private in controller

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,18 +5,15 @@ class StaticPagesController < ApplicationController
   def about
   end
 
+  def landing_page
+		@products = Product.limit(3)
+	end
+
 	def thank_you
 	  @name = params[:name]
 	  @email = params[:email]
 	  @message = params[:message]
 	  UserMailer.contact_form(@email, @name, @message).deliver_now
 	end
-
-	private
-
-		def landing_page
-			@products = Product.limit(3)
-		end
-
 
 end
