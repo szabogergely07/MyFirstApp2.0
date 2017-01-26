c04f37c626081acb820e4bf762f218fa480018f6
Greg <szabogergely07@gmail.com>
2017-01-26 12:14:52 +0100

    6.14 Create ActiveJob channels for each product and request authentication

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,8 @@ class CommentsController < ApplicationController
 	  @comment.user = current_user
 	  respond_to do |format|
 	  	if @comment.save
-	  		ActionCable.server.broadcast 'product_channel', comment: @comment, average_rating: @comment.product.average_rating
+	  		# ActionCable.server.broadcast 'product_channel', comment: @comment, average_rating: @comment.product.average_rating
+	  		ProductChannel.broadcast_to @product.id, comment: @comment, average_rating: @product.average_rating
 	  		format.html { redirect_to @product, notice:'Review was successfully created' }
 	  		format.json { render :show, status: :created, location: @product }
 	  		format.js
