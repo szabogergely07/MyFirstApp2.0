f9cbbea8abe9a1a2508207e60fc70fcddeaa4a09
Greg <szabogergely07@gmail.com>
2017-01-27 09:23:09 +0100

    6.14 Show comments to everyone on the specific product page with ActionCable

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,7 +6,7 @@ class CommentsController < ApplicationController
 	  respond_to do |format|
 	  	if @comment.save
 	  		# ActionCable.server.broadcast 'product_channel', comment: @comment, average_rating: @comment.product.average_rating
-	  		ProductChannel.broadcast_to @product.id, comment: @comment, average_rating: @product.average_rating
+	  		ProductChannel.broadcast_to @product.id, comment: CommentsController.render(partial: 'comments/comment', locals: {comment: @comment, current_user: current_user}), average_rating: @product.average_rating
 	  		format.html { redirect_to @product, notice:'Review was successfully created' }
 	  		format.json { render :show, status: :created, location: @product }
 	  		format.js
