757789d09326dcf36f859dd44c14ea2bc59b97d4
Greg <szabogergely07@gmail.com>
2017-07-30 12:42:02 +0200

    Add facebook auth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -271,6 +271,6 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
-  config.omniauth :facebook, "App ID", "App Secret", callback_url: "http://localhost:3000/users/auth/facebook/callback"
+  config.omniauth :facebook, "488997471438512", "fb2d60dfbfdd18cfefea9eb87da8bcba", callback_url: "http://localhost:3000/users/auth/facebook/callback"
 
 end
