0999a31d2137b2075ae701e6364b3c11a2b442ec
Greg <szabogergely07@gmail.com>
2017-07-30 14:56:21 +0200

    Test user authenticate

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!
+  # before_action :set_user, only: [:show, :edit, :update, :destroy]
+  # before_action :authenticate_user!
   load_and_authorize_resource
 
   # GET /users
@@ -74,19 +74,4 @@ class UsersController < ApplicationController
       params.require(:user).permit(:first_name, :last_name)
     end
 
-    def facebook
-      @user = User.from_omniauth(request.env["omniauth.auth"])
-
-      if @user.persisted?
-        sign_in_and_redirect @user, :event => :authentication
-        set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
-      else
-        session["devise.facebook_data"] = request.env["omniauth.auth"]
-        redirect_to new_user_registration_url
-      end
-    end
-
-    def failure
-      redirect_to root_path
-  end
 end
