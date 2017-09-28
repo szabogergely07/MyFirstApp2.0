3fa970ba169c5c3b65ab91394414fe751041bca8
Gergely Szabo <szabogergely07@gmail.com>
2017-09-28 17:18:39 +0000

    Remove foreign key

--- db/migrate/20170928171706_remove_foreign_key.rb
+++ db/migrate/20170928171706_remove_foreign_key.rb
@@ -1,4 +1,4 @@
-class UpdateForeignkeyInfoInPacts < ActiveRecord::Migration
+class RemoveForeignKey < ActiveRecord::Migration[5.0]
   def change
     add_foreign_key "comments", "products", on_delete: :nullify
   end
